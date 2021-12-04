23db437ede0b8d1c7d434b37ca0e29d965bbb1e5
Gregor Zeman <if19b210@technikum-wien.at>
2021-12-04 14:31:15 +0100

    Add project files.

---
+++ uniquePtr.cpp
@@ -0,0 +1,129 @@
+﻿#include "uniquePtr.h"
+
+
+template <class T>
+UniquePtr<T>::UniquePtr() : ptr(nullptr) 
+{
+}
+
+template <class T>
+UniquePtr<T>::UniquePtr(T* obj) : ptr(obj) //copy variable obj to ptr
+{
+}
+
+template <class T>
+UniquePtr<T>::~UniquePtr() {
+    this->release();
+}
+
+template <class T>
+typename UniquePtr<T>::pointer UniquePtr<T>::get()
+{
+    return this->ptr;
+}
+
+template <class T>
+typename UniquePtr<T>::class_type& UniquePtr<T>::operator*() const
+{
+    return *(this->ptr);
+}
+
+template <class T>
+typename UniquePtr<T>::pointer UniquePtr<T>::operator->() const
+{
+    return this->ptr;
+}
+
+template <class T>
+UniquePtr<T>::operator bool() const
+{
+    return this == nullptr;
+}
+
+template <class T>
+void UniquePtr<T>::reset()
+{
+
+}
+
+template <class T>
+void UniquePtr<T>::swap(T* other)
+{
+
+}
+
+template <class T>
+typename UniquePtr<T>::pointer UniquePtr<T>::release()
+{
+    T* tempPtr = this->ptr;
+	this->ptr = nullptr;
+    return tempPtr;
+}
+
+
+
+
+
+// constructor
+//template <class T>
+//UniquePtr<T>::UniquePtr() :
+//    _refCount(nullptr),
+//    _ptr(nullptr)
+//{
+//}
+//
+//template <class T>
+//UniquePtr<T>::UniquePtr(T* iObject) :
+//    _refCount(new RefCount()),
+//    _ptr(iObject)
+//{
+//    this->_refCount->AddRef();
+//}
+//
+//// copy constructor
+//template <class T>
+//UniquePtr<T>::UniquePtr(const UniquePtr<T>& iSPtr)
+//{
+//    this->_copyUniquePtr(iSPtr);
+//}
+//
+//// destructor
+//template <class T>
+//UniquePtr<T>::~UniquePtr() {
+//    this->_release();
+//}
+//
+//// operators
+//template <class T>
+//UniquePtr<T>& UniquePtr<T>::operator=(const UniquePtr<T>& iSPtr) {
+//    if (iSPtr._ptr && (this != &iSPtr)) {
+//        this->_release();
+//        this->_copyUniquePtr(iSPtr);
+//    }
+//    return *this;
+//}
+//
+//template <class T>
+//T& UniquePtr<T>::operator*() {
+//    return *(this->_ptr);
+//}
+//
+//template <class T>
+//T* UniquePtr<T>::operator->() {
+//    return this->_ptr;
+//}
+//
+//template <class T>
+//void UniquePtr<T>::_release() {
+//    if (this->_refCount && this->_refCount->Release() == 0) {
+//        delete this->_ptr;
+//        delete this->_refCount;
+//    }
+//}
+//
+//template <class T>
+//void UniquePtr<T>::_copyUniquePtr(const UniquePtr<T>& iSPtr) {
+//    this->_ptr = iSPtr._ptr;
+//    this->_refCount = iSPtr._refCount;
+//    this->_refCount->AddRef();
+//}
---
+++ uniquePtr.h
@@ -0,0 +1,70 @@
+﻿#pragma once
+
+#ifndef UNIQUE_PTR_H
+#define UNIQUE_PTR_H
+#include <memory>
+//Literature: https://www.informit.com/articles/article.aspx?p=25264
+
+template <class T>
+class UniquePtr  
+{
+public:
+	using class_type = T;
+	using pointer = T*;
+	pointer ptr;  //pointer to any data type we want
+
+	UniquePtr();
+	explicit UniquePtr(T* obj);
+	~UniquePtr();
+
+	pointer get();
+
+	class_type& operator *() const;
+	pointer operator->() const;
+	explicit operator bool() const;
+
+	static void swap(T* other);
+	pointer release();
+	static void reset();
+};
+
+
+
+#endif
+
+
+
+//class RefCount {
+//public:
+//    void AddRef() {
+//        ++(this->_count);
+//    }
+//    int Release() {
+//        return --(this->_count);
+//    }
+//private:
+//    int _count;
+//};
+
+//public:
+//    int element_type;
+//    UniquePtr();
+//    UniquePtr(T* iObject);
+//
+//    // copy constructor
+//    UniquePtr(const UniquePtr<T>& iSPtr);
+//
+//    // destructor
+//    ~UniquePtr();
+//
+//    // operators
+//    UniquePtr<T>& operator=(const UniquePtr<T>& iSPtr);
+//    T& operator*();
+//    T* operator->();
+//
+//private:
+//    T* _ptr;
+//    RefCount* _refCount;
+//
+//    void _release();
+//    void _copyUniquePtr(const UniquePtr<T>& iSPtr);
